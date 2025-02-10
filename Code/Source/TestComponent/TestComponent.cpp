499794338bdbc4c8ffaf7d4b2c64cce96da8deb6
Jan Hanca <jan.hanca@robotec.ai>
2025-02-10 18:21:17 +0100

    Test subscriber to print spawnable id
    Signed-off-by: Jan Hanca <jan.hanca@robotec.ai>

---
+++ Code/Source/TestComponent/TestComponent.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Robotec.ai.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include "TestComponent.h"
+#include <AtomLyIntegration/CommonFeatures/CoreLights/AreaLightBus.h>
+#include <AzCore/Component/TransformBus.h>
+#include <AzCore/Serialization/EditContext.h>
+#include <AzCore/Serialization/SerializeContext.h>
+#include <ROS2/Frame/ROS2FrameComponent.h>
+#include <ROS2/ROS2Bus.h>
+#include <ROS2/Utilities/ROS2Names.h>
+
+#include <AzFramework/Asset/GenericAssetHandler.h>
+#include <AzFramework/Spawnable/Spawnable.h>
+
+namespace JHO3DETestGem
+{
+    void TestComponent::Reflect(AZ::ReflectContext* context)
+    {
+        if (auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
+        {
+            serializeContext->Class<TestComponent>()
+                ->Version(0)
+                ->Field("SubscriberConfiguration", &TestComponent::m_subscriberConfiguration);
+
+            if (auto editContext = serializeContext->GetEditContext())
+            {
+                editContext->Class<TestComponent>("TestComponent", "TestComponent")
+                    ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
+                    ->Attribute(AZ::Edit::Attributes::Category, "JHO3DETestGem")
+                    ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("Game"))
+                    ->DataElement(
+                        AZ::Edit::UIHandlers::Default,
+                        &TestComponent::m_subscriberConfiguration,
+                        "Subscriber configuration",
+                        "ROS 2 subscriber configuration for printing id of the spawnable");
+            }
+        }
+    }
+
+    void TestComponent::GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required)
+    {
+        required.push_back(AZ_CRC_CE("ROS2Frame"));
+    }
+
+    void TestComponent::Activate()
+    {
+        auto ros2Node = ROS2::ROS2Interface::Get()->GetNode();
+        if (!ros2Node)
+        {
+            AZ_Error("TestComponent", false, "ROS2 node is not available. ROS 2 services will not be created.");
+            return;
+        }
+
+        auto ros2Frame = ROS2::Utils::GetGameOrEditorComponent<ROS2::ROS2FrameComponent>(GetEntity());
+        if (!ros2Frame)
+        {
+            AZ_Error("TestComponent", false, "ROS2 frame is not available. ROS 2 services will not be created.");
+            return;
+        }
+
+        m_subscriber = ros2Node->create_subscription<std_msgs::msg::String>(
+            ROS2::ROS2Names::GetNamespacedName(ros2Frame->GetNamespace(), m_subscriberConfiguration.m_topic).c_str(),
+            m_subscriberConfiguration.GetQoS(),
+            [this](const std_msgs::msg::String::SharedPtr msg)
+            {
+                const AZStd::string prefabPath = msg->data.c_str();
+                AZ_Error("JHDEBUG", false, "Subscriber called: %s", prefabPath.c_str());
+                HelperMethod(prefabPath);
+            });
+    }
+
+    void TestComponent::Deactivate()
+    {
+        m_subscriber.reset();
+    }
+
+    void TestComponent::HelperMethod(const AZStd::string& prefabPath)
+    {
+        AZ::IO::Path spawnablePath(prefabPath);
+        spawnablePath.ReplaceExtension("spawnable");
+        AZ::Data::AssetId spawnableId = AZ::Data::AssetId();
+        AZ::Data::AssetCatalogRequestBus::BroadcastResult(
+            spawnableId,
+            &AZ::Data::AssetCatalogRequestBus::Events::GetAssetIdByPath,
+            spawnablePath.c_str(),
+            azrtti_typeid<AzFramework::Spawnable>(),
+            false);
+
+        AZ_Error("JHDEBUG", false, "SpawnablePath %s", spawnablePath.c_str());
+        AZ_Error("JHDEBUG", false, "SpawnableId: %llu", spawnableId);
+
+    }
+} // namespace JHO3DETestGem
---
+++ Code/Source/TestComponent/TestComponent.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Robotec.ai.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#pragma once
+
+#include <AzCore/Component/Component.h>
+#include <AzCore/std/string/string.h>
+#include <ROS2/Communication/TopicConfiguration.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+namespace JHO3DETestGem
+{
+    class TestComponent
+        : public AZ::Component
+    {
+    public:
+        AZ_COMPONENT(TestComponent, "{98D5A3B5-D8C7-4D4F-8B75-4CBE47D05E08}");
+        TestComponent(){};
+        ~TestComponent(){};
+
+        static void Reflect(AZ::ReflectContext* context);
+
+        static void GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required);
+
+        void Activate() override;
+        void Deactivate() override;
+
+    private:
+        ROS2::TopicConfiguration m_subscriberConfiguration;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr m_subscriber;
+
+        void HelperMethod(const AZStd::string& prefabPath);
+    };
+} // namespace JHO3DETestGem
